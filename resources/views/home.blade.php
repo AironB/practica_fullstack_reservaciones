498c633dfc4b5b040b9916108bdb14bf456614f5
= <=>
2024-10-07 23:19:17 -0600

    primeros pasos con blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gestion de Alojamientos</title>
+</head>
+<body>
+    @php
+        $edad = 12;
+    @endphp
+        @if($edad>=18)
+        <h1>Sos un adulto</h1>;
+        @else
+        <h1>Estas Joven!</h1>;
+        @endif
+
+</body>
+</html>
